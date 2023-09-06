e3789091ead1cccac8ac0229eb2fc6b5312b6d27
TekaNik <tekateka6@gmail.com>
2023-09-06 17:28:28 +0100

    added index.js file and played with DOM

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById('black').onclick = codeBlack
+
+
+function codeBlack(){
+    // document.querySelector('*').style.backgroundColor = '#000';
+    document.querySelector('HTML').style.backgroundColor ='#000'
+    document.querySelector('*').style.color ='white'
+}
