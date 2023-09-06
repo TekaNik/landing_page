df3ab87ecdd492063bdade52a77cdeb2f9a8f1d0
TekaNik <tekateka6@gmail.com>
2023-09-07 00:11:21 +0100

    doing some test with JS

--- index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('black').onclick = codeBlack
-
-
-function codeBlack(){
-    // document.querySelector('*').style.backgroundColor = '#000';
-    document.querySelector('HTML').style.backgroundColor ='#000'
-    document.querySelector('*').style.color ='white'
-}
