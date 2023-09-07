b7570e22a26bfbf2c5a514f2bb1b9e98539c74c9
TekaNik <tekateka6@gmail.com>
2023-09-07 11:21:19 +0100

    working on 'call to action section'

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('black').onclick = dothis
+
+function dothis(){
+    document.querySelector('body').style.backgroundColor ='blue'
+    document.querySelector('body').style.color ='red'
+}
