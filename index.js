c001ebc411a060b925ab319d62ad145c754f3fea
TekaNik <tekateka6@gmail.com>
2023-09-10 13:09:38 +0100

    added some javascript code to be used in the future

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 document.getElementById('black').onclick = dothis
 
 function dothis(){
-    document.querySelector('body').style.backgroundColor ='blue'
-    document.querySelector('body').style.color ='red'
+    let name = prompt()
+    if(name =='admin'){
+        document.querySelector('body').style.backgroundColor ='blue'
+        document.querySelector('body').style.color ='red'
+
+    } else {
+        document.getElementById('black').innerHTML ="wrong Password"
+    }
+    
 }
